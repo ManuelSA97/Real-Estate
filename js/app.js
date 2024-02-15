faa19b467f8dd113660e07d75f42448e24086d89
Manuel Sandi <manuelsandi97@hotmail.com>
2024-02-14 20:43:23 -0600

    Agregado de efecto de scrolling al bg-position de las imagenes de las propiedades

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const imagenes = document.querySelectorAll('.propiedad__imagen')
+    //console.log(imagenes);
+
+window.addEventListener('scroll', () => {
+    const scroll = this.scrollY / -20;
+
+    imagenes.forEach((imagen) => {
+        imagen.style.backgroundPositionY = `${scroll}px`;
+    })
+});
+
